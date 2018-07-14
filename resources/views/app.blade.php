03aa50a531c3a9ec65e9c452fd6232b13c4eb5b6
Trevor Hebert <t.hebert73@gmail.com>
2018-07-14 00:49:27 -0400

    add google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -111,6 +111,17 @@
 
         <footer>
             <script async src="/js/app.js"></script>
+
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-59455628-3"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-59455628-3');
+            </script>
+
         </footer>
     </body>
 </html>
