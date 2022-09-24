7d06f3867589a2b3b6f40c36c13a2ed43e719a90
Trevor Hebert <t.hebert73@gmail.com>
2022-09-24 01:40:56 -0400

    site view tracking fix

--- app/Http/Controllers/Api/Admin/Tracking/TrackingController.php
+++ app/Http/Controllers/Api/Admin/Tracking/TrackingController.php
@@ -2,6 +2,7 @@
 
 namespace DAngelaCampaign\Http\Controllers\Api\Admin\Tracking;
 
+use Carbon\Carbon;
 use DAngelaCampaign\Http\Controllers\Controller;
 use DAngelaCampaign\Models\SiteView;
 use Illuminate\Http\Request;
@@ -19,6 +20,9 @@ class TrackingController extends Controller
     public function trackSiteView(Request $request)
     {
 
+        $siteViewInLast24Hours = SiteView::where('ip', request()->getClientIp())->where('created_at', '>=', Carbon::now()->subDay())->first();
+        if ($siteViewInLast24Hours) return;
+
         $siteViewData['referrer'] = $request->get('externalReferrer');
         $siteViewData['source'] = $request->get('source');
         $siteViewData['ip'] = request()->getClientIp();
@@ -26,6 +30,5 @@ class TrackingController extends Controller
         $siteView = new SiteView($siteViewData);
 
         $siteView->save();
-
     }
 }
